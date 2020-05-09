7732031f0261a6b49c2bd04e9a509315a5ea988f
Julio Rodriguez <juliorod18.08.97@gmail.com>
2020-05-09 11:44:54 -0500

    Crear incompleto

--- src/main/java/opcionX/Crear.java
+++ src/main/java/opcionX/Crear.java
@@ -27,36 +27,23 @@ public class Crear extends Menu {
 
     @Override
     public void realizarAccion(int opcion) {
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-
-            System.out.println("nombre: ");  // Output user input
-            String nombre = myObj.nextLine();  // Read user input
-
-            System.out.println("apellido: ");  // Output user input
-            String apellido = myObj.nextLine();  // Read user input
-
-            System.out.println("email: ");  // Output user input
-            String email = myObj.nextLine();  // Read user input
-
-            System.out.println("ciudad: ");  // Output user input
-            String ciudad = myObj.nextLine();  // Read user input
-
-            System.out.println("fecha_nacimiento: ");  // Output user input
-            String fecha_nacimiento = myObj.nextLine();  // Read user input
-
-            System.out.println("peso: ");  // Output user input
-            float peso = Float.parseFloat((myObj.nextLine()));
-
-            System.out.println("direccion_ip: ");  // Output user input
-            String direccion_ip = myObj.nextLine();  // Read user input
-
-            Persona p = new Persona(nombre, apellido, email, ciudad, fecha_nacimiento, peso, direccion_ip);
-            objectMapper.writeValue(new File("resources/personas.json"), p);
-        } catch (IOException ex) {
-            Logger.getLogger(Crear.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        ObjectMapper objectMapper = new ObjectMapper();
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("nombre: ");
+        String nombre = myObj.nextLine();
+        System.out.println("apellido: ");
+        String apellido = myObj.nextLine();
+        System.out.println("email: ");
+        String email = myObj.nextLine();
+        System.out.println("ciudad: ");
+        String ciudad = myObj.nextLine();
+        System.out.println("fecha_nacimiento: ");
+        String fecha_nacimiento = myObj.nextLine();
+        System.out.println("peso: ");
+        float peso = Float.parseFloat((myObj.nextLine()));
+        System.out.println("direccion_ip: ");
+        String direccion_ip = myObj.nextLine();
+        Persona p = new Persona(nombre, apellido, email, ciudad, fecha_nacimiento, peso, direccion_ip);
     }
 
 }
