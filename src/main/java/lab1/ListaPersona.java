18ee6cbf17176ecd1f45b3284c3ed3214dcc1c90
Julio Rodriguez <juliorod18.08.97@gmail.com>
2020-05-09 11:57:42 -0500

    Constructor

--- src/main/java/lab1/ListaPersona.java
+++ src/main/java/lab1/ListaPersona.java
@@ -4,6 +4,14 @@ import java.util.ArrayList;
 
 public class ListaPersona{
 
+    public ArrayList<Persona> getListaPersona() {
+        return listaPersona;
+    }
+
+    public void setListaPersona(ArrayList<Persona> listaPersona) {
+        this.listaPersona = listaPersona;
+    }
+
     ArrayList<Persona> listaPersona = new ArrayList<>();
 
 }
