553865a637378cef07998e03f9f74965760ba559
Rodrigo5023 <a20163458@pucp.edu.pe>
2020-05-09 11:16:44 -0500

    Funcion Fibonacci y Funcion Factorial

--- src/main/java/lab1/App.java
+++ src/main/java/lab1/App.java
@@ -1,7 +1,15 @@
 package lab1;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-    
+
+
+        Scanner sc = new Scanner(System.in);
+        while (true){
+
+
+        }
     }
 }
